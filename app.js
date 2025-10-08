938e82b371be5a3bf34ef6e01207f112c2a08a2e
Abdul Kadir (Technology - New Delhi) <abdul.kadir@partner.htmedialabs.com>
2025-10-08 09:26:08 +0530

    global middleware and req.url value

--- app.js
+++ app.js
@@ -1,18 +1,46 @@
 import express from "express";
 
-// console.log(express);
 const port = 7000;
 const app = express();
-// console.log(app);
 
-app.disable('x-powered-by')
-
-app.get('/', (req, res) => {
-  // res.setHeader();
-  res.end('hello world !!!!');
-  // res.send('Hello World !!');
+// this middleware will be called for each api call.
+app.use((req, res, next) => {
+  console.log('middleware')
+  // if we didn't call next method then api will be hanged.
+  next()
 })
 
+app.get('/',  (req, res) => {
+  res.send('Home Page');
+})
+
+app.get('/test', (req, res) => {
+  res.send('Test Page');
+})
+
+// --------------------------------------------------
+
+// app.use((req, res, next) => {
+//   console.log('middleware')
+//   console.log(req.url)
+//   console.log(req.route)
+//   // res.end('middleware')
+//   next()
+// })
+
+// app.get('/',  (req, res) => {
+//   console.log('home ')
+//   console.log(req.url)
+//   res.send('Home Page');
+// })
+
+// app.get('/test', (req, res) => {
+//   console.log('test')
+//   console.log(req.url);
+//   console.log(req.route)
+//   res.send('Test Page');
+// })
+
 app.listen(port, () => {
-  console.log(`server is ruuning on port ;; ${port}`);
+  console.log(`server is ruuning http://localhost:${port}`);
 })
